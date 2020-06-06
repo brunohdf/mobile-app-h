071f5561638516e65e71cc164b8f82ad405d6225
brunohdf <brunuhuu@gmail.com>
2020-06-06 18:14:24 -0300

    start new project

---
+++ app/src/main/java/com/brx/mobileapp/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.brx.mobileapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.brx.mobileapp.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, MainFragment.newInstance())
+                .commitNow()
+        }
+    }
+
+}
