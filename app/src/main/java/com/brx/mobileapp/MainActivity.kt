b056527b3b895d37cf8a635a0ae9fb775f6543eb
brunohdf <brunuhuu@gmail.com>
2020-06-07 14:01:21 -0300

    Add bottom navigation bar

--- app/src/main/java/com/brx/mobileapp/MainActivity.kt
+++ app/src/main/java/com/brx/mobileapp/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.brx.mobileapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.brx.mobileapp.ui.main.MainFragment
+import kotlinx.android.synthetic.main.main_activity.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -13,6 +15,30 @@ class MainActivity : AppCompatActivity() {
             supportFragmentManager.beginTransaction()
                 .replace(R.id.container, MainFragment.newInstance())
                 .commitNow()
+        }
+
+        setupNavigationBar()
+    }
+
+    private fun setupNavigationBar() {
+        bottom_navigation.itemIconTintList = null
+        bottom_navigation.setOnNavigationItemSelectedListener {
+            Toast.makeText(
+                this, "TODO: ${when (it.itemId) {
+                    R.id.home -> {
+                        "Home"
+                    }
+                    R.id.map -> {
+                        "Map"
+                    }
+                    else -> {
+                        "Profile"
+                    }
+                }}", Toast.LENGTH_SHORT
+            ).show()
+
+
+            true
         }
     }
 
